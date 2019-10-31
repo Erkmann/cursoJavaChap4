d89f8a6495c4a8f51059a3bc7ca0551516934768
Erkmann <erkmann08@gmail.com>
2019-10-31 16:22:52 -0300

    aula 23 2min

---
+++ src/teste_processamento.java
@@ -0,0 +1,15 @@
+public class teste_processamento {
+
+    public static void main(String[] args){
+
+        int x, y;
+
+        x = 5;
+
+        y = x * 2;
+
+        System.out.println(y);
+
+    }
+
+}
