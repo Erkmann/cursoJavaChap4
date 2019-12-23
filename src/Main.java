72ae284c2bb3448adf394d0a9d802a167a631ba0
Erkmann <erkmann08@gmail.com>
2019-12-23 11:51:29 -0300

    entrada de dados com a linha toda

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
-import java.util.Locale;
+import java.util.Scanner;
 
-public class Main {
-
+public class Main{
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.0;
+		int x;
+		String s1, s2, s3;
 
-		System.out.printf("%s tem %d anos e a renda eh de %.2fR$%n", nome, idade, renda);
+		x = sc.nextInt();
+		sc.nextLine();
+		s1 = sc.nextLine();
+		s2 = sc.nextLine();
+		s3 = sc.nextLine();
 
+		System.out.println("DADOS DIGITADOS: ");
+		System.out.println(x);
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+
+		sc.close();
 	}
-
 }
